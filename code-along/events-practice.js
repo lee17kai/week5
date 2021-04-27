d2d30ba13df8499b783078d4f85490daa56214ff
lee17kai <81439325+lee17kai@users.noreply.github.com>
2021-04-27 09:45:53 -0500

    945

--- code-along/events-practice.js
+++ code-along/events-practice.js
@@ -9,19 +9,41 @@ function getRandomInt(max) {
 // RECIPE (algorithm) – do not modify the HTML!
 
 // Store a reference to the roll button in memory
+let rollButton = document.querySelector(`.roll-button`)
 
 // Listen for the clicking of the roll button, when clicked:
-
+rollButton.addEventListener(`click`, async function(event) {
   // - Ignore the roll button's default behavior
+  event.preventDefault()
 
   // - Store a random integer in memory, representing the roll of the first die
-  
+  let roll1 = getRandomInt(6)
+
   // - Store a random integer in memory, representing the roll of the second die
+  let roll2 = getRandomInt(6)
 
   // - Store the total of the two random numbers, representing the total roll
+  let total = roll1 + roll2
 
   // - Replace the first die image using the first random value (hint: setAttribute)
-
+  if(roll1 = 1) {
+    image.setAttribute(`src`, `../images/1.png`)
+  }
+  else if(roll = 2) {
+    image.setAttribute(`src`, `../images/2.png`)
+  }
+  else if(roll = 3) {
+    image.setAttribute(`src`, `../images/3.png`)
+  }
+  else if(roll = 4) {
+    image.setAttribute(`src`, `../images/4.png`)
+  }
+  else if(roll = 5) {
+    image.setAttribute(`src`, `../images/5.png`)
+  }
+  else {
+    image.setAttribute(`src`, `../images/6.png`)
+  }
   // - Replace the second die image using the second random value (hint: setAttribute)
 
   // - Store a reference to the player name input element
@@ -35,3 +57,4 @@ function getRandomInt(max) {
     // - Store a reference to the "result" un-ordered list element
 
     // - Insert the sentence to the "result" un-ordered list as a list item (li)
+})
