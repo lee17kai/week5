0b44674370ebcf37aa37a54a5174f261dc46b1d2
lee17kai <81439325+lee17kai@users.noreply.github.com>
2021-04-27 08:59:00 -0500

    859

--- code-along/events.js
+++ code-along/events.js
@@ -1,19 +1,52 @@
-// Get a reference to the page title element from the DOM
-let pageTitleElement = document.querySelector(`.page-title`)
+// // Get a reference to the page title element from the DOM
+// let pageTitleElement = document.querySelector(`.page-title`)
 
-// Modify the page title element's contents
-pageTitleElement.innerHTML = `Movies to watch!`
+// // Modify the page title element's contents
+// pageTitleElement.innerHTML = `Movies to watch!`
 
-// Get a reference to the bulleted list of movies
+// // Get a reference to the bulleted list of movies
+// let movieList = document.querySelector(`.movies-to-watch`)
+
+// // Add an new list item to the bulleted list of movies
+// movieList.insertAdjacentHTML(`beforeend`, `
+//   <li>Spaceballs</li>
+// `)
+
+// // Get a reference to the image
+// let image = document.querySelector(`img`)
+
+// // Add the `border-pink-500` class to the image element
+// image.classList.add(`border-pink-500`)
+
+//Get reference to the click me button in memory
+let clickMeButton = document.querySelector(`.click-me`)
+
+//Event listener for click me button. add event listner
+//takes 2 arguments(what event listening for (in this case/usually `click`), async function thing {put stuff in curly brace})
+//alert is a thing. 
+clickMeButton.addEventListener(`click`, async function(event) {
+
+  //this shows you a little pop up with "the page says, the button was clicked"
+  alert(`the button was clicked`)
+
+  //This changes the title of the page (per code above in 1-2, 4-5)
+  let pageTitleElement = document.querySelector(`.page-title`)
+  pageTitleElement.innerHTML = `Movies to watch!`
+})
+
+//Get reference to "add movie" link
+let addMovieLink = document.querySelector(`.add-movie`)
+// Event listener for the "add movie" link
+addMovieLink.addEventListener(`click`, async function(event) {
+
+  //prevent link's default behavior (which is to go to Google)
+  event.preventDefault()
+  
+  // Get a reference to the bulleted list of movies
 let movieList = document.querySelector(`.movies-to-watch`)
 
 // Add an new list item to the bulleted list of movies
 movieList.insertAdjacentHTML(`beforeend`, `
-  <li>Spaceballs</li>
-`)
-
-// Get a reference to the image
-let image = document.querySelector(`img`)
-
-// Add the `border-pink-500` class to the image element
-image.classList.add(`border-pink-500`)
+  <li>Spaceballs</li>`)
+})
+//
